d33ade6ba3ec8a0ae8980bd09c0824dac059532e
Vijay <vijaykrishnach268@gmail.com>
2025-04-22 22:11:34 +0530

    Commit

---
+++ HI/src/main/java/com/us/cal/gov/HiApplication.java
@@ -0,0 +1,13 @@
+package com.us.cal.gov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiApplication.class, args);
+	}
+
+}
